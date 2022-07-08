d6b2eb572270ec4216b3099f2d2da81c12d1c679
Jakelyne Lyra <jaklyra@gmail.com>
2022-07-08 18:13:28 -0300

    Primeiro Commit - IDE Intellij

---
+++ src/br/com/jaklyra/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.jaklyra;
+
+import br.com.jaklyra.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args){
+
+       Gato gato = new Gato();
+
+       System.out.println(gato);
+
+    }
+
+}
---
+++ src/br/com/jaklyra/model/Gato.java
@@ -0,0 +1,73 @@
+package br.com.jaklyra.model;
+
+import java.util.Objects;
+
+public class Gato {
+
+    private String nome;
+
+    private String cor;
+
+    private Integer idade;
+
+    public Gato() {
+
+
+    }
+
+    //CONSTRUTOR DA CLASSE
+    public Gato(String nome, String cor, Integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    //MÉTODOS GETTER E SETTER
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    //IGUALS E HASHCODE
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
