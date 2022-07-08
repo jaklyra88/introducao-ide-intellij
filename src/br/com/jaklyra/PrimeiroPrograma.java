7f73928f0c69d9eace52f1776d6755761be9de08
Jakelyne Lyra <jaklyra@gmail.com>
2022-07-08 18:22:01 -0300

    Criação da classe Livro

--- src/br/com/jaklyra/PrimeiroPrograma.java
+++ src/br/com/jaklyra/PrimeiroPrograma.java
@@ -9,6 +9,50 @@ public class PrimeiroPrograma {
 
        System.out.println(gato);
 
+
+
+       Livro livro1 = new Livro("O problema dos 3 corpos",300);
+
+       System.out.println(livro1);
+
     }
 
+}
+
+class Livro {
+
+    private String nome;
+
+    private Integer numpaginas;
+
+    public Livro(String nome, Integer numpaginas){
+
+        this.nome = nome;
+
+        this.numpaginas = numpaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpaginas() {
+        return numpaginas;
+    }
+
+    public void setNumpaginas(Integer numpaginas) {
+        this.numpaginas = numpaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpaginas=" + numpaginas +
+                '}';
+    }
 }
